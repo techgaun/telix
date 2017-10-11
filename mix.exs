b5c507cf55026cbdd46d8bc04f6b3508c7995458
techgaun <coolsamar207@gmail.com>
2017-10-11 14:50:32 -0500

    initial deps

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Telix.Mixfile do
   use Mix.Project
 
+  @version "0.1.0-dev"
+
   def project do
     [
       app: :telix,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Telit API Client For Elixir",
+      name: "Telix",
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -21,8 +27,25 @@ defmodule Telix.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 0.13"},
+      {:ex_doc, "~> 0.17", only: :docs},
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Telix",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/techgaun/telix"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Samar Acharya"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/techgaun/telix"},
+      files: ~w(mix.exs README.md lib config)
     ]
   end
 end
